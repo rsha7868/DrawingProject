5a4df8a105de530d685ed43e0db8599ee8aae49a
Sharp <rsha7868@740s-pl-127.local>
2018-03-28 13:22:49 -0600

    making the creation of the art drawing board. on JPanel and JFrame.

--- src/art/view/ArtPanel.java
+++ src/art/view/ArtPanel.java
@@ -45,6 +45,74 @@ public class ArtPanel extends JPanel
 	
 	public ArtPanel(DrawingController app)
 	{
+		super();
+		this.app = app;
+		appLayout = newSpringLayout();
 		
+		currentScale = MINMUM_SCALE;
+		currentEdgeCount = MINMUM_EDGE;
+		scaleSlider = new JSlider(MINIMUM_SCALE, MAXINUM_SCALE);
+		edgeSlider = new JSlider(MINIMUM_EDGE, MAXIMUM_EDGE);
+		
+		canvas = new drawingCanvas(app);
+		sliderPanel = new JPanel();
+		buttonPanel = new JPanel(new GridLayout(0,1));
+		
+		triangleButton = new JButton("add Triangle");
+		rectangleButton = new JButton("add rectangle");
+		ellipseButton = new JButton("add ellipse");
+		polygonButton = new JButton("add polygon");
+		clearButton = new JButton("clear image");
+		saveButton = new JButton("save image");
+		colorButton = new JButton("change color");
+		
+		setupSliders();
+		setupPanel();
+		setupLayout();
+		setupListeners();
+		
+	}
+	private void setupSliders()
+	{
+		HashTable<Integer, JLable> scaleLabels = new HashTable<Integer, JLable>();
+		HashTable<Integer, JLable> edgeLabels = new HashTable<Integer, JLable>();
+		
+		scaleLabels.put(MINIMUM_SCALE, new JLable("<HTML>Small<BR>Shape</HTML>"));
+		ScaleLabels.put(MAXIMUM_SCALE + MINIMUM_SCALE)/ 2, new JLable("\"<HTML>Medium<BR>Shape</HTML>\""));
+		scaleLabels.put(MINIMUM_SCALE, new JLable("<HTML>Large<BR>Shape</HTML>"));
+		
+		edgeLabels.put(MiNINUM_EDGE, new JLabel("Edges: " + MINIMUM_EDGE));
+		edgeLabels.put(MAXINUM_EDGE, new JLabel("Edges: " + MAXIMUM_EDGE));
+		
+		ScaleSLider.setLabelTable(scaleLable);
+		ScaleSLider.setOrientation(JSlider.VERTICAL);
+		ScaleSLider.setSnapTTicks(true);
+		ScaleSLider.setMajarTickSpacing(3);
+		ScaleSLider.setMinorTickSpacing(1);
+		ScaleSLider.setPaintTicks(true);
+		ScaleSLider.setPaintLabels(true);
+	}
+	private void setupPanel()
+	{
+		this.setLayout(appLayout);
+		this.setBackground(Color.DARK_GREY);
+		this.setPerferredSize(new Dimension(1020, 768));
+		this.add(canvas);
+		
+		buttonPanel.setPreferredSize(new Dimension(200, 450));
+		buttonPanel.add(triangleButton);
+		buttonPanel.add(rectangleButton);
+		buttonPanel.add(ellipseButton);
+		buttonPanel.add(polygonButton);
+		buttonPanel.add(clearButton);
+		buttonPanel.add(colorButton);
+		buttonPanel.add(saveButton);
+		
+		sliderPanel.setPreferredSize(new Dimension(250, 450));
+		sliderPanel.add(scaleSlider);
+		sliderPanel.add(edgeSlider);
+		
+		this.add(buttonPanel);
+		this.add(sliderPanel);
 	}
 }
--- src/art/view/DrawingCanvas.java
+++ src/art/view/DrawingCanvas.java
@@ -1,6 +1,40 @@
 package art.view;
 
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.JPanel;
+
+import art.controller.DrawingController;
+
+
 public class DrawingCanvas
 {
-
+	private ArrayList<Polygon> TriangleList;
+	private ArrayList<Polygon> polgonList;
+	private ArrayList<Ellipse> ellipseList;
+	private ArrayList<Rectangle> rectangleList;
+	private DrawingController app;
+	
+	private BufferedImage canvasImage;
+	
+	public DrawingCanvas(DrawingController app)
+	{
+		super();
+		
+	}
+	
 }
